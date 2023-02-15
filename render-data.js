e369ada1fa6880d4c4c6746795fe3aeeafa4573f
Nguyễn Huy Hùng <nguyen.huy.hung-c@sun-asterisk.com>
2023-02-15 22:20:14 +0700

    add fakerjs package

---
+++ render-data.js
@@ -0,0 +1,29 @@
+const { faker } = require('@faker-js/faker');
+const fs = require('fs')
+
+const randomClassList = (n) => {
+  const classList = []
+  if(n <= 0) return []
+
+  Array.from(new Array(n)).forEach(() => {
+    const _class = {
+      id: faker.datatype.uuid(),
+      totalStudent: faker.datatype.number(40),
+      supervising_Teacher: `${faker.name.lastName()} ${faker.name.firstName()}`,
+      avatar_Teacher: faker.image.avatar(400, 400)
+    }
+    classList.push(_class)
+  })
+
+  return classList
+}
+
+(() => {
+  const classList = randomClassList(2)
+  const db = {
+    class: classList
+  }
+  fs.writeFile('./db.json', JSON.stringify(db), () => {
+    console.log('Write successfully')
+  })
+})()
